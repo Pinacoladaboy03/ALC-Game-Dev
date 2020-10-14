9fb903c9ed8cd36c674aac6e7c76a10eff1e4ae0
Jackson Cottam <jackson.c965@stu.nebo.edu>
2020-10-14 09:10:30 -0600

    Plane Program
    Completed Plane Program

---
+++ Flying 'N' Stuff Pt.1/Assets/Challenge 1/Scripts/RotatePropellerX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePropellerX : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    private float propellorSpeed = 1000;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, propellorSpeed * Time.deltaTime);
+    }
+}
