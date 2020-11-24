8545352c97851a5e95e528db48356f7983695735
Jackson Cottam <jackson.c965@stu.nebo.edu>
2020-11-24 09:10:25 -0700

    Jump Game.3
    I did some animations and stuffs.

--- Jump Game Pt.1/Assets/Scripts/PlayerController.cs
+++ Jump Game Pt.1/Assets/Scripts/PlayerController.cs
@@ -8,38 +8,49 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float jumpForce;
     public float gravityModifier;
+    private Animator playerAnim;
     
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     public bool isOnGround = true;
+    public ParticleSystem dirtParticle;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
         playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         isOnGround = false;
+        playerAnim.SetTrigger("Jump_trig");
+        dirtParticle.Stop();
         }
     }
     
     public bool gameOver = false;
+    public ParticleSystem explosionParticle;
     
     private void OnCollisionEnter(Collision collision)
     {
-    if (collision.gameObject.CompareTag("Ground")) 
-    {
-    isOnGround = true; 
-    }
-    else if (collision.gameObject.CompareTag("Obstacle"))
-    {
-    gameOver = true;
-    Debug.Log("Game Over!");
-    }
+        if (collision.gameObject.CompareTag("Ground")) 
+        {
+        isOnGround = true; 
+        dirtParticle.Play();
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        dirtParticle.Stop();
+        {
+        gameOver = true;
+        Debug.Log("Game Over!");
+        playerAnim.SetBool("Death_b", true);
+        playerAnim.SetInteger("DeathType_int", 1); 
+        explosionParticle.Play();
+        }
     }
 }
